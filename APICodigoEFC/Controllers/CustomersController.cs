156e91e857ec6e567ceea36d309b4f0d71981dd3
JUAN CUCHO <jcucho85@gmail.com>
2024-07-11 13:48:59 -0500

    implementacion de api de products

--- APICodigoEFC/Controllers/CustomersController.cs
+++ APICodigoEFC/Controllers/CustomersController.cs
@@ -20,17 +20,26 @@ namespace APICodigoEFC.Controllers
         [HttpGet]
         public List<Customer> Get(string? name, string? documentNumber) {
 
-            var query = _context.Customers.Where(
-                                                    x=>x.Name.Contains(name.Trim()) 
-                                                    || x.DocumentNumber.Contains(documentNumber.Trim())
-                                                    );
-            return query.ToList();
+            //var query = _context.Customers.Where(
+            //                                        x=>x.Name.Contains(name.Trim()) 
+            //                                        || x.DocumentNumber.Contains(documentNumber.Trim())
+            //                                        );
+            //return query.ToList();
             //if (!string.IsNullOrEmpty(name))
             //    query = query.Where(x => x.Name.Contains(name));
 
 
 
             //return _context.Customers.ToList();
+            IQueryable<Customer> query = _context.Customers;
+            if (!string.IsNullOrEmpty(name))
+                query = query.Where(x => x.Name.Contains(name));
+
+            if (!string.IsNullOrEmpty(documentNumber))
+                query = query.Where(x => x.DocumentNumber.Contains(documentNumber));
+
+            return query.ToList();
+
         }
 
         [HttpPost]
